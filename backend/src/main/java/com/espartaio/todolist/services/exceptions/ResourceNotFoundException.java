78349fd4178116e56512f3168ab917fd441bbc89
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-02-10 17:34:04 -0400

    services execeptions and resources exceptions

---
+++ backend/src/main/java/com/espartaio/todolist/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.espartaio.todolist.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+	}
+}
