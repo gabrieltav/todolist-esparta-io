ae2613b72010c4599abfa5d56554f647a87e4351
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-02-14 22:32:55 -0400

    teste de logica euclidean

---
+++ teste_de_logica_euclidean/src/teste_de_logica_euclidean/Solution.java
@@ -0,0 +1,16 @@
+package teste_de_logica_euclidean;
+
+class Solution {
+	public int solution(int N, int M) {
+        int[] X = new int[N];
+        int i = 0;
+        int count = 0;
+        while(X[i] == 0) {
+            X[i] = 1;
+            i = (i + M) % N;            
+            count++;
+        }
+        return count;
+    }
+
+}
