4bc8c160850101226c03eb950ba954fc6034f248
Gabriel Tavares <tavaresgabriel963@gmail.com>
2022-02-14 23:13:47 -0400

    fix:consultas

--- estruturas-de-dados-e-sql/insert-tabelase-consulta.sql
+++ estruturas-de-dados-e-sql/insert-tabelase-consulta.sql
@@ -20,6 +20,8 @@ INSERT INTO disciplina (nome) VALUES ('Fisica');
 INSERT INTO disciplina (nome) VALUES ('Quimica');
 INSERT INTO disciplina (nome) VALUES ('Biologia');
 INSERT INTO disciplina (nome) VALUES ('Portugues');
+INSERT INTO disciplina (nome) VALUES ('Educacao fisica');
+INSERT INTO disciplina (nome) VALUES ('Banco de Dados');
 
 INSERT INTO turma (dia_da_semana, horario, PROFESSOR_id, DISCIPLINA_id, ALUNO_id) VALUES ('Segunda-feira','07:00|08:00',1,1,1);
 INSERT INTO turma (dia_da_semana, horario, PROFESSOR_id, DISCIPLINA_id, ALUNO_id) VALUES ('Terça-feira','08:00|09:00',2,2,2);
@@ -33,11 +35,33 @@ INSERT INTO aluno_turma (ALUNO_id, TURMA_id) VALUES (1,1);
 INSERT INTO aluno_turma (ALUNO_id, TURMA_id) VALUES (2,1);
 INSERT INTO aluno_turma (ALUNO_id, TURMA_id) VALUES (3,1);
 INSERT INTO aluno_turma (ALUNO_id, TURMA_id) VALUES (4,1);
+INSERT INTO aluno_turma (ALUNO_id, TURMA_id) VALUES (1,2);
+INSERT INTO aluno_turma (ALUNO_id, TURMA_id) VALUES (2,2);
+INSERT INTO aluno_turma (ALUNO_id, TURMA_id) VALUES (3,3);
 
 
+
+--Buscar os nomes de todos os alunos que frequentam alguma turma do professor 'JOAO PEDRO'.
 SELECT turma.PROFESSOR_id
 FROM turma
 INNER JOIN aluno_turma on turma.id = aluno_turma.TURMA_id
 inner JOIN professor on turma.PROFESSOR_id = professor.id
 where professor.nome = 'Joao Pedro'
 
+-- Buscar os dias da semana que tenham aulas da disciplina 'MATEMATICA'.
+SELECT turma.dia_da_semana
+FROM turma
+INNER JOIN disciplina on turma.disciplina_id = disciplina.id
+where disciplina.nome = 'matematica'
+
+-- Buscar todos os alunos que frequentem aulas de 'MATEMATICA' e também 'FISICA'.
+SELECT *
+FROM turma
+INNER JOIN disciplina on turma.disciplina_id = disciplina.id
+where disciplina.nome = 'fisica' and 'matematica'
+
+-- Buscar as disciplinas que não tenham nenhuma turma.
+SELECT *
+FROM turma
+INNER JOIN disciplina on turma.disciplina_id != disciplina.id
+where disciplina.nome != turma.disciplina_id
